3b58a5ff44c400c073cd5e510ef4cfdbcb8cd6cb
slavasubot <slavasubot@gmail.com>
2015-07-24 08:30:16 +0300

    Add entities (account and user)

---
+++ Market.Domain/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Market.Domain.Entities
+{
+   public class Account
+    {
+        public int Id { get; set; }      
+        public string Descriptions { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+       
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Market.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Market.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        public DateTime JoinDate { get; set; }
+        public ICollection<Account> Accounts { get; set; }
+        public ICollection<Category> Categories { get; set; } 
+    }
+}
