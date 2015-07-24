9d42e0bbc7b9eeb3447e373f1c4d3eee1f83e7ff
slavasubot <slavasubot@gmail.com>
2015-07-24 08:46:56 +0300

    Add UserService

---
+++ Market.Services/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Market.Domain.Entities;
+
+namespace Market.Services.Services
+{
+    public interface IUserService
+    {
+        void Create(User user, string password, string confirmPassword);
+        void Activate(string userId, string code);
+        void Update(User user);
+        void Delete(User user);
+        void ChangePassword(string email, string oldPassword, string newPassword, string confirmNewPassword);
+        void ResetPassword(string userId, string code, string newPassword, string confirmNewPassword);
+        User FindUser(string email, string password);
+        User GetByEmail(string email);
+
+        bool CheckEmail(string email);
+    }
+}
---
+++ Market.Services/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using Market.Data.Repositories;
+using Market.Domain.Entities;
+
+
+namespace Market.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Create(User user, string password, string confirmPassword)
+        {
+            _userRepository.Create(user, password);
+        }
+
+        public void Activate(string userId, string code)
+        {
+            _userRepository.ConfirmEmail(userId, code);
+        }
+
+        public void Update(User user)
+        {
+            _userRepository.Update(user);
+        }
+
+        public void Delete(User user)
+        {
+            _userRepository.Delete(user);
+        }
+
+        public void ChangePassword(string email, string oldPassword, string newPassword, string confirmNewPassword)
+        {
+            _userRepository.ChangePassword(email, oldPassword, newPassword);
+        }
+
+        public void ResetPassword(string userId, string code, string newPassword, string confirmNewPassword)
+        {
+            _userRepository.ResetPassword(userId, code, newPassword);
+        }
+
+        public User FindUser(string email, string password)
+        {
+            return _userRepository.FindUser(email, password);
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _userRepository.GetByEmail(email);
+        }
+
+        public bool CheckEmail(string email)
+        {
+            return _userRepository.CheckEmail(email);
+        }
+    }
+}
